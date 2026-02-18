5dd6ce5a51d02f94c9476057b3214c119409addb
Chu A. <chu.amk22@gmail.com>
2026-02-18 17:36:38 +0100

    feat: Implement Logo component

---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,14 @@
+const Logo = () => {
+  return (
+    <div className="text-xl font-bold text-gray-800 whitespace-nowrap">
+      <img
+        src="./logo.png"
+        alt="E-Buy Logo"
+        className="w-20 h-20 mr-2 inline-block"
+      />
+      <p className="inline-block align-middle">E-Buy</p>
+    </div>
+  );
+};
+
+export default Logo;
