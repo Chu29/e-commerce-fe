136a7c2642ed52bd6be05a58f61db2f8eba9cc10
Chu A. <chu.amk22@gmail.com>
2026-02-19 14:08:05 +0100

    fix: Adjust Logo component styles for improved responsiveness

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -1,10 +1,10 @@
 const Logo = () => {
   return (
-    <div className="text-xl font-bold text-gray-800 whitespace-nowrap">
+    <div className="text-lg sm:text-xl font-bold text-gray-800 whitespace-nowrap">
       <img
         src="./logo.png"
         alt="E-Buy Logo"
-        className="w-20 h-20 mr-2 inline-block"
+        className="w-12 h-12 sm:w-20 sm:h-20 mr-2 inline-block"
       />
       <p className="inline-block align-middle">E-Buy</p>
     </div>
