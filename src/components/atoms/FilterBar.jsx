259c8d498676950650492fd0774a47d31ce321da
Chu A. <chu.amk22@gmail.com>
2026-02-19 14:08:16 +0100

    feat: Update FilterBar to fetch categories dynamically from API

--- src/components/atoms/FilterBar.jsx
+++ src/components/atoms/FilterBar.jsx
@@ -1,10 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchCategories } from "../../services/category.service";
+
 const FilterBar = () => {
-  const options = ["All Categories", "Electronics", "Clothing", "Books"];
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+  if (isLoading) console.log("Loading categories");
+  if (isError) return console.log("Error fetching categories");
+
+  const options = data?.categories.map((category) => category.name) || [];
   return (
     <select
       name="category"
       id="filter"
-      className="rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm text-gray-700 outline-none cursor-pointer hover:border-gray-300 transition-colors"
+      className="rounded-lg border border-gray-200 bg-white px-2 sm:px-3 py-2 text-xs sm:text-sm text-gray-700 outline-none cursor-pointer hover:border-gray-300 transition-colors shrink-0"
     >
       {options.map((option) => (
         <option key={option} value={option}>
