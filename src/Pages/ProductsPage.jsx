f397a71a3f3766d43590169b16e5b34b30aed096
Chu A. <chu.amk22@gmail.com>
2026-02-22 13:17:30 +0100

    feat: Update ProductPage to include Logo, SearchBar, and FilterBar in Header

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -1,10 +1,19 @@
+import FilterBar from "../components/atoms/FilterBar";
+import Logo from "../components/atoms/Logo";
 import Header from "../components/molecules/Header";
 import Main from "../components/molecules/Main";
+import SearchBar from "../components/organisms/SearchBar";
 
 const ProductPage = () => {
   return (
     <>
-      <Header />
+      <Header>
+        <Logo />
+        <div className="flex items-center gap-3 w-full sm:flex-1 sm:max-w-2xl">
+          <SearchBar />
+          <FilterBar />
+        </div>
+      </Header>
       <Main />
     </>
   );
