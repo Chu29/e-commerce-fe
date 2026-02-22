4574839e3046cda03e459b0a0980cec6b286dbca
Chu A. <chu.amk22@gmail.com>
2026-02-22 12:39:18 +0100

    feat: Create ProductPage component to display Header and Main components

---
+++ src/Pages/ProductsPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/molecules/Header";
+import Main from "../components/molecules/Main";
+
+const ProductPage = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default ProductPage;
