0399d708062e9fa9508780678b377f1f23820c69
Chu A. <chu.amk22@gmail.com>
2026-02-23 12:56:47 +0100

    feat: Add route for CreateProductPage to enable product creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import ProductPage from "./Pages/ProductsPage";
 import LandingPage from "./Pages/LandingPage";
+import ProductForm from "./components/NewProductForm";
+import CreateProductPage from "./Pages/CreateProductPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" Component={LandingPage} />
       <Route path="/products" Component={ProductPage} />
+      <Route path="/products/new" Component={CreateProductPage} />
     </Routes>
   );
 }
