9e43d8e9bab2ee39b7e816b091b389b8bef432cd
Chu A. <chu.amk22@gmail.com>
2026-02-23 10:20:57 +0100

    feat: Add LandingPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import ProductPage from "./Pages/ProductsPage";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" Component={LandingPage} />
       <Route path="/products" Component={ProductPage} />
     </Routes>
   );
