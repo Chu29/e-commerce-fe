444c2d60603ef458699142d845b495c18efff45d
Chu A. <chu.amk22@gmail.com>
2026-05-11 14:57:00 +0100

    feat: Add route for CreateNewCategoryPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./Pages/LandingPage";
 import CreateProductPage from "./Pages/CreateProductPage";
 import EditProductPage from "./Pages/EditProductPage";
 import ProductDetailsPage from "./Pages/ProductDetailsPage";
+import CreateNewCategoryPage from "./Pages/CreateNewCategoryPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" Component={LandingPage} />
       <Route path="/products" Component={ProductPage} />
       <Route path="/products/new" Component={CreateProductPage} />
+      <Route path="/categories/new" Component={CreateNewCategoryPage} />
       <Route path="/products/:id/edit" Component={EditProductPage} />
       <Route path="/products/:id/details" Component={ProductDetailsPage} />
     </Routes>
