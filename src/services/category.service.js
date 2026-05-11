0e25d20dcf22009d81aabdfad65c33a76ae75804
Chu A. <chu.amk22@gmail.com>
2026-05-11 14:55:39 +0100

    feat: Implement createCategory function to handle category creation

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -9,3 +9,14 @@ export const fetchCategories = async () => {
     throw new Error("Failed to fetch categories");
   }
 };
+
+export const createCategory = async (categoryData) => {
+  try {
+    const response = await api.post("categories", { json: categoryData });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    const errorResponse = await error.response?.json().catch(() => null);
+    throw new Error(errorResponse?.error || "Failed to create category");
+  }
+};
